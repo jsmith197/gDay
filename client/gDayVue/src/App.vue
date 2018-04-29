4aa93a9620e5a5062a2ef80b36a4c664ad295fe7
Job <jobsmith9@gmail.com>
2018-04-29 13:08:57 -0600

    inital commit

---
+++ client/gDayVue/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Headder></Headder>
+  </div>
+</template>
+
+<script>
+import Headder from './components/Headder';
+
+export default {
+  name: 'App',
+  components: {
+    Headder,
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ client/gDayVue/src/components/Headder.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="headder">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Headder',
+  data() {
+    return {
+      msg: 'gDay',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
