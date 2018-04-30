e1378b3e236c2a2590d4728e14e70d2952e65f4f
Clark Newell <clarknewell@Clarks-MacBook-Pro.local>
2018-04-29 19:39:50 -0600

    tyring heroku again

--- server/app.js
+++ server/app.js
@@ -18,8 +18,8 @@ const express     = require('express')
 const bodyParser  = require('body-parser')
 const morgan      = require('morgan')
 const cors        = require('cors')
-const knex        = require('./connection.js')
-const app         = module.exports = express()
+// const knex        = require('./connection.js')
+const app         = express()
 const port        = process.env.PORT || 3000
 
 app.get('/',(request,response)=> response.send('success'))
@@ -30,22 +30,22 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(morgan(process.env.NODE_ENV !== 'production' ? 'dev' : 'combined'))
 app.use(cors({origin: true, credentials: true}))
 
-app.use(notFound)
-app.use(errorHandler)
+// app.use(notFound)
+// app.use(errorHandler)
 
-function notFound(req, res, next) {
-  const url = req.originalUrl
-  if (!/favicon\.ico$/.test(url) && !/robots\.txt$/.test(url)) {
-    console.error('[404: Requested file not found] ', url)
-  }
-  res.status(404).send({error: 'Url not found', status: 404, url})
-}
+// function notFound(req, res, next) {
+//   const url = req.originalUrl
+//   if (!/favicon\.ico$/.test(url) && !/robots\.txt$/.test(url)) {
+//     console.error('[404: Requested file not found] ', url)
+//   }
+//   res.status(404).send({error: 'Url not found', status: 404, url})
+// }
 
-function errorHandler(err, req, res, next) {
-  console.error('ERROR', err)
-  const stack =  process.env.NODE_ENV !== 'production' ? err.stack : undefined
-  res.status(500).send({error: err.message, stack, url: req.originalUrl})
-}
+// function errorHandler(err, req, res, next) {
+//   console.error('ERROR', err)
+//   const stack =  process.env.NODE_ENV !== 'production' ? err.stack : undefined
+//   res.status(500).send({error: err.message, stack, url: req.originalUrl})
+// }
 
 app.listen(port)
   .on('error',     console.error.bind(console))
