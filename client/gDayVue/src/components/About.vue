e18f913735f760679b40c1565cf14f9347c0f32a
James Truitt <tru@rockyagirl.com>
2018-05-04 15:42:22 -0600

    my final commit before presentation

--- client/gDayVue/src/components/About.vue
+++ client/gDayVue/src/components/About.vue
@@ -1,31 +1,23 @@
 <template>
   <div class="container">
-    <div class='row'>
-      <div class='col s12 m5'>
-        <div class='card-panel teal'>
-          <h3 class='one'>Our Plan.</h3>
-          <p>With the gDay web application our team sought to create a platform for bringing together gSchool <strong>alumni, developers,</strong> and <strong>support staff</strong>, for an annual two-day extravaganza — filled with tech talks, guest speakers, hackathons, product demos, workshops, an alumni picnic, and happy hour. We created this app to invoke a vision of what an official 'Galvanize Alumni Day' might look like.</p>
-        </div>
-      </div>  
+    <div>
+      <br>
+      <img class='about-photo' src='../assets/g-platte-gt.jpg'/>
     </div>
-    <div class='row'>
-      <div class='col s12 m5'>
-        <div class='card-panel teal'>
-          <h3 class='two'>The Tradition.</h3>
-          <p>A huge part of the <strong>Galvanize experience</strong> is about networking with your peers and being involved in the tech community. Let's continue that tradition, post-graduation, by curating an opportunity to reconnect with friends, re-engage with our dynamic network of blossoming and seasoned professionals, and of course, celebrating the wondrous experience    in which we all share.</p>
-        </div>
-      </div>
+    <div>
+      <h3 class='one'>Our Plan.</h3>
+      <p>With the gDay web application our team sought to create a platform for bringing together gSchool <strong>alumni, developers,</strong> and <strong>support staff</strong>, for an annual two-day extravaganza — filled with tech talks, guest speakers, hackathons, product demos, workshops, an alumni picnic, and happy hour. We created this app to invoke a vision of what an official 'Galvanize Alumni Day' might look like.</p>
     </div>
-    <div class='row'>
-      <div class='col s12 m5'>
-        <div class='card-panel teal'>
-          <h3 class='three'>Be Inspired.</h3>
-          <p>gDay is more than a reunion, it’s about appreciating and continuing to enhance your professional growth, while being introduced to new concepts and technologies! Workshops will range from topics like DevOps and UX design, to "what's new in tech," and even the hottest new tech being covered in the curriculum Galvanize curriculum.</p>
-          <br/>
-          <p>Please submit your event inqueries and speaker suggestions <strong><a class='link' href="#">here!</a></strong></p>
-          <br/>
-        </div>
-      </div>
+    <div>
+      <h3 class='two'>The Tradition.</h3>
+      <p>A huge part of the <strong>Galvanize experience</strong> is about networking with your peers and being involved in the tech community. Let's continue that tradition, post-graduation, by curating an opportunity to reconnect with friends, re-engage with our dynamic network of blossoming and seasoned professionals, and of course, celebrating the wondrous experience    in which we all share.</p>
+    </div>
+    <div>
+      <h3 class='three'>Be Inspired.</h3>
+      <p>gDay is more than a reunion, it’s about appreciating and continuing to enhance your professional growth, while being introduced to new concepts and technologies! Workshops will range from topics like DevOps and UX design, to "what's new in tech," and even the hottest new tech being covered in the curriculum Galvanize curriculum.</p>
+      <br/>
+      <p>Please submit your event inqueries and speaker suggestions <strong><a class='link' href="#">here!</a></strong></p>
+      <br/>
     </div>
   </div>
 </template>
@@ -36,9 +28,9 @@ export default {
   data() {
     return {
       title: 'About'
-    };
+    }
   }
-};
+}
 </script>
 
 <style scoped>
@@ -49,6 +41,10 @@ export default {
 
 .container {
   text-align: left;
+}
+
+.about-photo {
+  align-self: center;
 }
 
 p {
